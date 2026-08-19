68fe029f43702d1f00d0a9fbcb203e1c898084ac
XM666-Dev <x94732568960@163.com>
2026-08-19 10:48:14 +0800

    Fix method finding

--- src/main/java/com/xm666/combateventfix/handler/BetterAttackHandler.java
+++ src/main/java/com/xm666/combateventfix/handler/BetterAttackHandler.java
@@ -1,5 +1,23 @@
 package com.xm666.combateventfix.handler;
 
+import net.minecraft.client.Minecraft;
+
+import java.lang.reflect.Method;
+
 public class BetterAttackHandler {
     public static boolean isBetterCombatAttack;
+    private static Method betterCombatAttackMethod;
+
+    public static Method getBetterCombatAttackMethod() {
+        if (betterCombatAttackMethod != null) return betterCombatAttackMethod;
+
+        for (var method : Minecraft.class.getDeclaredMethods()) {
+            if (!method.getName().matches("^handler\\$[^$]+\\$bettercombat\\$pre_doAttack$")) continue;
+
+            betterCombatAttackMethod = method;
+            return method;
+        }
+
+        throw new NullPointerException();
+    }
 }
